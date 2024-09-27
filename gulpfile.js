5cb131169641cee16a9bdbb16c17d125134ae311
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-27 13:02:58 +0700

    update gulp sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,38 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('sass'); // Official Dart SASS package
+const through2 = require('through2'); // Stream utility
 const path = require('path');
 const fs = require('fs');
 
-function buildStyles() {
+// Custom function to compile SASS using through2
+function compileSass() {
   const includePaths = [
     path.join(__dirname, 'node_modules'),
     path.join(__dirname, '../node_modules'),
     path.join(__dirname, '../../node_modules'),
     path.join(__dirname, '../../../node_modules')
   ].filter(fs.existsSync);
+  return through2.obj(function (file, _, cb) {
+    if (file.isBuffer()) {
+      try {
+        // Compile SASS file
+        const result = sass.compile(file.path, { style: 'expanded', includePaths });
+
+        // Update file contents with the compiled CSS
+        file.contents = Buffer.from(result.css);
+        file.path = file.path.replace(path.extname(file.path), '.css'); // Change extension to .css
+      } catch (err) {
+        return cb(new Error(`SASS compilation failed: ${err.message}`));
+      }
+    }
+    cb(null, file); // Pass the file along the stream
+  });
+}
+
+function buildStyles() {
   return gulp
     .src('./src/sass/**/*.scss')
-    .pipe(
-      sass({
-        outputStyle: 'expanded',
-        includePaths
-      }).on('error', sass.logError)
-    )
+    .pipe(compileSass()) // Custom compilation function
     .pipe(gulp.dest('./source/style'));
 }
 
